a2debb055b7f8df404775453101c6f140386d02e
hughes <hughes@udel.edu>
2026-05-07 08:04:20 -0400

    mobile change 5.0

--- header.php
+++ header.php
@@ -17,7 +17,10 @@
                     </a>
                 </div>
                 <div id="header-logo-title">
-                    <a id="logo-title-link" href="/">DELAWARE ENVIRONMENTAL OBSERVING SYSTEM</a>
+                    <a id="logo-title-link" href="/" aria-label="DELAWARE ENVIRONMENTAL OBSERVING SYSTEM">
+                        <span class="title-desktop">DELAWARE ENVIRONMENTAL OBSERVING SYSTEM</span>
+                        <span class="title-mobile">DELAWARE ENV.<br>OBSERVING SYSTEM</span>
+                    </a>
                 </div>
             </div>
             <!-- Removed mobile menu icon code -->
